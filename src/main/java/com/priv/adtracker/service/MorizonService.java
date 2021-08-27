52634411192dab19f26a18f2a23e6fd45dbd3f45
Łukasz <abramowicz.lukasz94@gmail.com>
2021-08-27 10:06:13 +0200

    MORIZON working

--- src/main/java/com/priv/adtracker/service/MorizonService.java
+++ src/main/java/com/priv/adtracker/service/MorizonService.java
@@ -2,14 +2,18 @@ package com.priv.adtracker.service;
 
 import com.priv.adtracker.model.Advertisment;
 import com.priv.adtracker.model.Url;
+import com.priv.adtracker.registry.AdvertismentsRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -18,9 +22,38 @@ public class MorizonService {
 
     @PostConstruct
     public List<Advertisment> getAdvertisments() throws IOException {
+        log.info("Scanning {}", Url.MORIZON);
+        List<Advertisment> advertisments = new ArrayList<>();
         Document doc = Jsoup.connect(Url.MORIZON.getUrl()).get();
 
-        return null;
+        Elements elements = doc.select("section.single-result__content.single-result__content--height");
+
+        for (Element element : elements) {
+            try {
+                String price = element.select("p.single-result__price").get(0).text();
+                String area = element.select("ul.param.list-unstyled.list-inline li").text();
+                String pricePerMeter = element.select("p.single-result__price").get(1).text();
+                String location = element.select("h2.single-result__title").text();
+                String link = element.select("a").attr("href");
+
+                Advertisment advertisment = Advertisment.builder()
+                        .link(link)
+                        .area(area)
+                        .location(location)
+                        .price(price)
+                        .pricePerMeter(pricePerMeter)
+                        .build();
+
+                advertisments.add(advertisment);
+            } catch (Exception e) {
+                log.error("Error while scanning MORIZON: {}", e.toString());
+                continue;
+            }
+        }
+
+        AdvertismentsRegistry.updateRegistry(advertisments);
+
+        return advertisments;
     }
 
     @Scheduled(cron = "0 5,35 * * * *")
